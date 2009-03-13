56db1e9aa5fdaf01270a135b1679af260d564fc7
dmitry.vysochin <dmitry.vysochin@1be09bee-0f26-11de-a6f9-19e1259300d8>
2009-03-13 09:49:25 +0000

    Добавил SDK

---
+++ trunk/sb_smpp_sdk/sb_smpp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sb_pdu
+from smpputil import *
+
+
+class bind_transmitter(sb_pdu.__sb_bind__,sb_pdu.__sb_pdu_header__ ):
+    def __init__(self,system_id,password,system_type="cpa",interface_version=0x00000034,addr_ton=0x00,addr_npi=0x00,addr_range=""):
+	sb_pdu.__sb_bind__.__init__(self,system_id,password,system_type,interface_version,addr_ton,addr_npi,addr_range,smpp_command_id["bind_transmitter"])
+	
+class bind_receiver(sb_pdu.__sb_bind__):
+    def __init__(self,system_id,password,system_type="cpa",interface_version=0x00000034,addr_ton=0x00,addr_npi=0x00,addr_range=""):
+	sb_pdu.__sb_bind__.__init__(self,system_id,password,system_type,interface_version,addr_ton,addr_npi,addr_range,smpp_command_id["bind_receiver"])
+	
+class bind_transceiver(sb_pdu.__sb_bind__,sb_pdu.__sb_pdu_header__):
+    def __init__(self,system_id,password,system_type="cpa",interface_version=0x00000034,addr_ton=0x00,addr_npi=0x00,addr_range=""):
+	sb_pdu.__sb_bind__.__init__(self,system_id,password,system_type,interface_version,addr_ton,addr_npi,addr_range)
+
+class bind_transceiver_resp(sb_pdu.__sb_bind_resp__):
+    pass
+    
+def class_from_pck(pck,path=""):
+    header = sb_pdu.__sb_pdu_header__.__reverce__(pck)
+    
+    return  get_smppclass(get_smppclass_name(header),path).__reverce__(pck,header)
+    
